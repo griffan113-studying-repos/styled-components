b2d9a4d8e9fa749db2850489a5fcd5d9fcd8de72
Eliel M. Gaspar <elielmichel@gmail.com>
2021-01-15 19:28:45 -0300

    Aula 5 (Final)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/ui/Themes";
+import themeOff from "./assets/images/themeOn.svg";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/globalStyle";
+import { BtnTema, Icone } from "./Components/ui/index.js";
+import SwitchTema from "./Components/SwitchTema/index.jsx";
 
 function App() {
+  const [tema, setTema] = useState(true);
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  };
+
   return (
     <>
-      <GlobalStyle />
-      <Cabecalho />
-      <Container />
+      <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
+        <GlobalStyle />
+        <BtnTema onClick={toggleTema}>
+          {/* <SwitchTema tema={tema} /> */}
+          <Icone src={themeOff} alt="" />
+        </BtnTema>
+        <Cabecalho />
+        <Container />
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/Components/SwitchTema/index.jsx
@@ -0,0 +1,9 @@
+/* import React from "react";
+import ThemeOn from "../../assets/images/themeOn.svg";
+import themeOff from "../../assets/images/themeoff.svg";
+import { Icone } from "../ui";
+
+const claro = <Icone src={hemeOn} alt="Tema Claro" />;
+const escuro = <Icone src={themeOff} alt="Tema Escuro" />;
+
+export default ({ tema }) => (tema ? escuro : claro); */
